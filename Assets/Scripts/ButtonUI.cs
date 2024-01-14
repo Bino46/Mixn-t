76bb6a9e86127db7810bccf65baaa4ac82979e5c
Kitsuto <clement.terrier@e-artsup.net>
2024-01-14 15:07:56 +0100

    Add prefab UI

--- Assets/Scripts/ButtonUI.cs
+++ Assets/Scripts/ButtonUI.cs
@@ -1,23 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject HUD;
+    public GameObject Pause;
+    public GameObject TitleScreen;
+    public Slider VolumeSlider;
+
+    public void PauseGame()
     {
-        
+        HUD.SetActive(false);
+        Pause.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ContinueGame()
     {
-        
+        HUD.SetActive(true);
+        Pause.SetActive(false);
+
     }
 
-    public void ahahahahah()
+    public void MainMenu()
     {
-        Debug.Log("prout");
+        TitleScreen.SetActive(true);
+        HUD.SetActive(false);
+        Pause.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Volume()
+    {
+        AudioListener.volume = VolumeSlider.value;
+    }
+
+    public void Start()
+    {
+        TitleScreen.SetActive(false);
+        HUD.SetActive(true);
     }
 }
